ffa0c111d3977d1e2dfb23526985d32032776d1b
OmarChouchane <omar.ch52831@gmail.com>
2026-05-05 16:35:29 +0100

    Initial commit

---
+++ run_email_outreach.bat
@@ -0,0 +1,14 @@
+@echo off
+setlocal
+
+cd /d "%~dp0"
+set "PYTHON_EXE=%LOCALAPPDATA%\Programs\Python\Python311\python.exe"
+
+if not exist "%PYTHON_EXE%" (
+    echo Python 3.11 was not found at:
+    echo %PYTHON_EXE%
+    pause
+    exit /b 1
+)
+
+"%PYTHON_EXE%" "%~dp0email_outreach_app.py"
